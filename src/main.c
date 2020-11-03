205548994ac1c4803780c55735fdd4191af34a81
solnah <solnahosh@gmail.com>
2020-11-03 14:31:09 +0700

    Added rules to src/main.c

--- src/main.c
+++ src/main.c
@@ -4,28 +4,33 @@
 
 #include "func.h"
 
-int main(void)
-{
-    int num = generate_number();
-    int input = 0, steps = 0, win = 0;
+int main(void) {
+  printf("Guess a four-digit number.\n");
+  printf("Numbers can't be repeated.\n");
+  printf(
+      "The cow is the guessed number of the\npuzzle that is in the wrong "
+      "place.\n");
+  printf("Bull is a number that stands in its place.\n");
+  int num = generate_number();
+  int input = 0, steps = 0, win = 0;
 
-    while (input != num && win == 0) {
-        printf("\nGuess: ");
-        scanf("%d", &input);
+  while (input != num && win == 0) {
+    printf("\nGuess: ");
+    scanf("%d", &input);
 
-        if (check_input(input)) {
-            print_info(num, input);
-        }
-        getchar();
+    if (check_input(input)) {
+      print_info(num, input);
+    }
+    getchar();
 
-        if (input == num) {
-            win = 1;
-        }
-
-        steps++;
+    if (input == num) {
+      win = 1;
     }
 
-    if (win == 1) {
-        printf("Congratulations!\nYou won in %d chances ", steps);
-    }
+    steps++;
+  }
+
+  if (win == 1) {
+    printf("Congratulations!\nYou won in %d chances ", steps);
+  }
 }
