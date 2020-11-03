69c5514367814f21edc377b6204fe873c6ba13d3
Cradhin <kirya.rahmatulin@gmail.com>
2020-11-03 21:37:10 +0700

    Changed the structure to src/main.c

--- src/main.c
+++ src/main.c
@@ -4,15 +4,15 @@
 
 #include "func.h"
 
-int main(void) 
+int main(void)
 {
-  printf("Guess a four-digit number.\n");
-  printf("Numbers can't be repeated.\n");
-  printf("The cow is the guessed number of the\npuzzle that is in the wrong "
-      "place.\n");
-  printf("Bull is a number that stands in its place.\n");
-  int num = generate_number();
-  int input = 0, steps = 0, win = 0;
+    printf("Guess a four-digit number.\n");
+    printf("Numbers can't be repeated.\n");
+    printf("The cow is the guessed number of the\npuzzle that is in the wrong "
+           "place.\n");
+    printf("Bull is a number that stands in its place.\n");
+    int num = generate_number();
+    int input = 0, steps = 0, win = 0;
 
     while (input != num && win == 0) {
         printf("\nGuess: ");
@@ -31,6 +31,6 @@ int main(void)
     }
 
     if (win == 1) {
-     printf("Congratulations!\nYou won in %d chances ", steps);
+        printf("Congratulations!\nYou won in %d chances ", steps);
     }
 }
